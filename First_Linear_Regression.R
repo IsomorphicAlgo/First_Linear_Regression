ed38017b0c0e3e1fb3480d9277f338bff6457ddb
IsomorphicAlgo <137707855+IsomorphicAlgo@users.noreply.github.com>
2024-07-06 18:57:22 -0700

    Brought in data from Kaggle for NVDA Historical Stock Price Information. Tester model for open and close price.

--- First_Linear_Regression.R
+++ First_Linear_Regression.R
@@ -1,1 +1,16 @@
-test <- 3
+# Libraries
+library(tidyverse)
+
+# Data courtesy of https://www.kaggle.com/datasets/prajwaldongre/nvidia-corp-share-price-2000-2024/data
+
+#Testing Data
+
+# read in the dataset
+nvda_data <- read.csv("G:/My Drive/Academia/MSDS/Machine Learning Specialization/DTSA 5509 Introduction to Machine Learning - Supervised Learning/First_Lin_Reg/First_Linear_Regression/NVDA.csv");
+head(nvda_data)
+
+close_open_model <- lm(Close ~ Open, data = nvda_data)
+
+# Print the model summary
+summary(close_open_model)
+
